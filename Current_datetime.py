c2b684ce8da3818d26bf6f69cf5cc7910b1fef8b
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-17 23:11:32 +0530

    Create Current_datetime.py

---
+++ Current_datetime.py
@@ -0,0 +1,7 @@
+from datetime import datetime   # we can import datetime from datetime module to get date and time. This module comes built into python.
+
+now=datetime.now()
+print(now)
+
+Output:
+2020-06-17 23:08:08.073200
