9bd1a1ba47b7c43b26c979538f511babb7a5b626
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-17 23:01:55 +0530

    Create Python_version.py

---
+++ Python_version.py
@@ -0,0 +1,8 @@
+import sys
+print(python version:)
+print(sys.version)
+
+Output:
+Python version:                                                                                                
+3.5.2 (default, Sep 10 2016, 08:21:44)                                                                        
+[GCC 5.4.0 20160609]   
