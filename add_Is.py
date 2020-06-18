11eff7c8c5a41913df3f1acb9912546ceee1c473
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-19 00:19:51 +0530

    Create add_Is.py

---
+++ add_Is.py
@@ -0,0 +1,15 @@
+s=input("Input a string:")
+if len(s)>=2 and s[:2]=="Is":
+    print(s)
+else:
+    s="Is"+s
+    print(s)
+    
+    
+output:
+python add_Is.py
+
+Input a string:sakshi
+Issakshi
+Input a string:Isempty
+Isempty
