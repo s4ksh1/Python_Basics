f14b8c96bd62c18f1842a5319b6999a10ea56f1e
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-18 09:34:25 +0530

    Create list-tuples.py

---
+++ list-tuples.py
@@ -0,0 +1,14 @@
+data=input("Enter few comma seperated numbers:")
+list=data.split(",")
+tuple=tuple(list)
+print("List is:"+str(list) )
+print("Tuple is:"+str(tuple))
+
+
+Output:
+python list-tuples.py
+
+Enter few comma seperated numbers:3,1,8,7,4,6
+List is:['3', '1', '8', '7', '4', '6']
+Tuple is:('3', '1', '8', '7', '4', '6')
+
