c0463fcf81f343693699f912ad2b0d4be4876870
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-18 09:37:17 +0530

    Create file_extension.py

---
+++ file_extension.py
@@ -0,0 +1,11 @@
+file=input("Enter any file name:")     # the code accepts the filename and print the file extension.
+list=file.split(".")
+print(list[1])
+
+
+Output:
+python file_extension.py
+
+Enter any file name:abc.py
+py
+
