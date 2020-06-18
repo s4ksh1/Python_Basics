c8f6bc007eb1798600d52f37bc5337049a576af8
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-19 00:26:00 +0530

    Create odd_or_even.py

---
+++ odd_or_even.py
@@ -0,0 +1,15 @@
+a = int(input("Enter any integer:"))
+if a%2==0:
+    print("The number you entered is even.")
+else:
+    print("The number you entered is odd.")
+    
+    
+Output:
+python odd_or_even.py
+Enter any integer:4
+The number you entered is even.
+
+python odd_or_even.py
+Enter any integer:9
+The number you entered is odd.
