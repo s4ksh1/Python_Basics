f5872096a5272b7e50a60dcff4600c9db785d9cd
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-18 09:41:04 +0530

    Create print_first_last.py

---
+++ print_first_last.py
@@ -0,0 +1,9 @@
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0],color_list[-1])                   # list starts with the index 0 and -1 refers to the last element of the list if we 
+                                                      # want to access the list in reverse order.
+
+
+Output:
+python print_first_last.py
+
+Red Black
