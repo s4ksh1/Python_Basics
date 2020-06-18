4674083818ed25a77cfc479a814b089b346f4321
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-18 10:12:39 +0530

    Create Calender.py

---
+++ Calender.py
@@ -0,0 +1,20 @@
+import calendar
+m= int(input("Enter month:"))
+y= int(input("Enter year:"))
+print()
+print(calendar.month(y,m))
+
+
+Output:
+python calender.py
+
+Enter month:6
+Enter year:2020
+
+     June 2020
+Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
