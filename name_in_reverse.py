8be38bea6a787d537c1e7cf06df157530966b78d
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-18 09:33:02 +0530

    Create name_in_reverse.py

---
+++ name_in_reverse.py
@@ -0,0 +1,12 @@
+first=input("Enter your first name:")
+last=input("Enter your last name:")
+print(last+" "+first)
+
+
+Output:
+python name_in_reverse.py
+
+Enter your first name:Sakshi
+Enter your last name:Aggarwal
+Aggarwal Sakshi
+
