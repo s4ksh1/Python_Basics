59413ea6fc88c97af0532b18bf79ed8e560e313d
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-19 00:21:30 +0530

    Create difference.py

---
+++ difference.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number:"))
+if a>17:
+    print(2*(a-17))
+else:
+    print(17-a)
+    
+
+Output:
+python differences.py
+
+Enter a number:7
+10
+
+python difference.py
+Enter a number:19
+4
