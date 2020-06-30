a7cb5204c2fa86e61c026ba99a08b2bd11e3df4f
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-30 18:00:28 +0530

    Create empty_or_not.py

---
+++ Lists/empty_or_not.py
@@ -0,0 +1,24 @@
+l = []
+if len(l)==0:
+    print("List is empty")
+else:
+    print("List is not empty")
+    
+    
+Output:
+python empty_or_not.py
+List is empty
+
+
+
+l = [1,2,3,4,5]
+if len(l)==0:
+    print("List is empty")
+else:
+    print("List is not empty")
+    
+
+Output:
+python empty_or_not.py
+List is not empty
+    
