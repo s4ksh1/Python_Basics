4038a43c592db39d955ac42a653e3ebf7eaf0459
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-30 17:18:41 +0530

    Create multiplication_of_elements.py

---
+++ Lists/multiplication_of_elements.py
@@ -0,0 +1,26 @@
+l = list()
+n = int(input("Enter the number of elements you want in list:"))
+for i in range(n):
+    a = int(input("Enter element to insert in the list:"))
+    l.append(a)
+
+print("Your list is:", l)
+
+#sum of elements of list
+M=1
+for x in l:
+    M = M*x
+
+print("Multiplication of elements is :",M)
+
+
+Output:
+python multiplication_of_ele.py
+Enter the number of elements you want in list:5
+Enter element to insert in the list:5
+Enter element to insert in the list:4
+Enter element to insert in the list:3
+Enter element to insert in the list:2
+Enter element to insert in the list:1
+Your list is: [5, 4, 3, 2, 1]
+Multiplication of elements is : 120
