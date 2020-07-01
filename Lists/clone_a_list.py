f23fa47dd5ac091ec48528238737b3fdd6573ff7
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-07-01 16:31:43 +0530

    Create clone_a_list.py

---
+++ Lists/clone_a_list.py
@@ -0,0 +1,10 @@
+my_list = [1,2,3,4,5]
+print("List is :",my_list)
+new_list = my_list.copy()
+print("Copied list is :",new_list)
+
+
+Output:
+python copy_list.py
+List is : [1, 2, 3, 4, 5]
+Copied list is : [1, 2, 3, 4, 5]
