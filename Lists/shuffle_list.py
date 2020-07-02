205f57b617e69c68a270c0b72396ef1d6d2dd5f3
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-07-02 21:25:55 +0530

    Create shuffle_list.py

---
+++ Lists/shuffle_list.py
@@ -0,0 +1,9 @@
+from random import shuffle        # it will return a shuffled list
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+shuffle(color_list)
+print(color_list)
+
+
+Output:
+python shuffle_list.py
+['Green', 'White', 'Yellow', 'Black', 'Red', 'Pink']
