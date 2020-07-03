ae2126e0c41ad398b2289433c0382a318d375ffd
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-07-03 16:24:57 +0530

    Create list_of_uniques.py

---
+++ Lists/list_of_uniques.py
@@ -0,0 +1,10 @@
+l = [5,2,9,7,6,4,9,9,9,1,1,2,6,5,4,4,3,10]
+print("Original list :",l)
+s = set(l)
+new_list = list(s)
+print("List of unique elements :",new_list)
+
+
+Output:
+Original list : [5, 2, 9, 7, 6, 4, 9, 9, 9, 1, 1, 2, 6, 5, 4, 4, 3, 10]
+List of unique elements : [1, 2, 3, 4, 5, 6, 7, 9, 10]
