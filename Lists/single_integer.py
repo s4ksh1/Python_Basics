59b83c27b414b22e3c6958257efac4b28f9482c2
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-07-04 11:28:40 +0530

    Create single_integer.py

---
+++ Lists/single_integer.py
@@ -0,0 +1,10 @@
+L = [76, 83, 50]
+print("Original List: ",L)
+x = int("".join(map(str, L)))
+print("Single Integer: ",x)
+
+
+Output:
+python single_integer.py
+Original List:  [76, 83, 50]
+Single Integer:  768350
